45ebffe29324af3f0582d0c6d8a5203a1f43e432
Amanda Costa <amandacostacs@yahoo.com>
2026-02-02 18:44:22 -0300

    feat: cria repositório de tarefas em memória

--- src/main/java/com/project/repository/TaskRepository.java
+++ src/main/java/com/project/repository/TaskRepository.java
@@ -1,4 +1,24 @@
 package com.project.repository;
 
+import com.project.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskRepository {
+
+    private final List<Task> tasks = new ArrayList<>();
+
+    public List<Task> findAll() {
+        return tasks;
+    }
+
+    public void save(Task task) {
+        tasks.add(task);
+    }
+
+    public void delete(int index) {
+        tasks.remove(index);
+    }
+
 }
