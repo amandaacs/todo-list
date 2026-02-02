b0f318a842e389ba539578efc913da59a51975a6
Amanda Costa <amandacostacs@yahoo.com>
2026-02-02 19:09:43 -0300

    feat: adiciona utilitários de data e arquivo

--- src/main/java/com/project/util/DateUtils.java
+++ src/main/java/com/project/util/DateUtils.java
@@ -1,4 +1,15 @@
 package com.project.util;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtils {
+
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+    public static LocalDate parseDate(String value){
+        return LocalDate.parse(value, FORMATTER);
+    }
 }
--- src/main/java/com/project/util/FileUtils.java
+++ src/main/java/com/project/util/FileUtils.java
@@ -1,4 +1,15 @@
 package com.project.util;
 
+import java.io.*;
+
 public class FileUtils {
+
+    public static BufferedReader reader(String file) throws IOException {
+        return new BufferedReader(new FileReader(file));
+    }
+
+    public static BufferedWriter writer(String file) throws IOException {
+        return new BufferedWriter(new FileWriter(file));
+    }
+
 }
