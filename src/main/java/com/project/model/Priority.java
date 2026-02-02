ca0f25e35693d74dcca44e8d1a1003cc4c63d5c5
Amanda Costa <amandacostacs@yahoo.com>
2026-02-02 18:40:48 -0300

    feat: adiciona modelo de Task, Status e Priority

--- src/main/java/com/project/model/Priority.java
+++ src/main/java/com/project/model/Priority.java
@@ -1,4 +1,16 @@
 package com.project.model;
 
-public class Priority {
+public enum Priority {
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5);
+
+    private  final  int level;
+
+    private Priority(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
 }
--- src/main/java/com/project/model/Task.java
+++ src/main/java/com/project/model/Task.java
@@ -1,4 +1,70 @@
 package com.project.model;
 
+import java.time.LocalDate;
+
 public class Task {
+
+    private String name;
+    private String description;
+    private LocalDate dueDate;
+    private Priority priority;
+    private Status status;
+    private String category;
+
+    public Task(String name, String description, LocalDate dueDate, Priority priority, Status status, String category) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
